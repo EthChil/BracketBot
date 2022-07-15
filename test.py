e2427ee614932fa01e9f4579fa09b5dfd652aba6
EthChil <echilder@uwaterloo.ca>
2022-07-15 15:47:21 -0400

    additional test cases

--- test.py
+++ test.py
@@ -2,6 +2,27 @@ import CommunicationHandler as comm
 
 log = comm.logger()
 
-spi = comm.SPIObject(0,0,log)
-
+spi = comm.SPIObject(0,0,log,mode=0b00)
 print(spi.readByte(0b0000000))
+
+spi = comm.SPIObject(0,1,log,mode=0b00)
+print(spi.readByte(0b0000000))
+
+spi = comm.SPIObject(1,0,log,mode=0b00)
+print(spi.readByte(0b0000000))
+
+spi = comm.SPIObject(1,1,log,mode=0b00)
+print(spi.readByte(0b0000000))
+
+spi = comm.SPIObject(0,0,log,mode=0b11)
+print(spi.readByte(0b0000000))
+
+spi = comm.SPIObject(0,1,log,mode=0b11)
+print(spi.readByte(0b0000000))
+
+spi = comm.SPIObject(1,0,log,mode=0b11)
+print(spi.readByte(0b0000000))
+
+spi = comm.SPIObject(1,1,log,mode=0b11)
+print(spi.readByte(0b0000000))
+
