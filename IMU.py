45bda37a7a02dbf77103003d429cba4cf1aad8f7
EthChil <echilder@uwaterloo.ca>
2022-07-28 01:29:33 -0400

    update IMU program to include gravity getter and init

--- IMU.py
+++ IMU.py
@@ -1,7 +1,62 @@
 import CommunicationHandler as comm
+import time
 
 log = comm.logger()
 
 IMU = comm.I2CObject(0,log)
 
 print(IMU.readByte(40))
+
+setup = 0
+
+def setupIMU():
+    global setup
+
+    #mode information
+    #NDOF mode - sensor fusion mode which allows us to access the gravity vector
+
+    #write RST_SYS in SYS_TRIGGER
+    IMU.writeByte(int("3F", 16), int("20", 16))
+
+    time.sleep(0.03)
+
+    #write power mode in PWR_MODE
+    IMU.writeByte(int("3E", 16), int("00", 16))
+
+
+    #write RST_SYS in SYS_TRIGGER
+    IMU.writeByte(int("3F", 16), int("00", 16))
+
+    time.sleep(0.03)
+
+    #Set operation mode in OPR_MODE (this should be NDOF)
+    IMU.writeByte(int("3D", 16), int("0C", 16))
+
+    setup = 1
+
+
+#Gravity vector is a 3D vector with 100 LSB equal to 1m/s^2 (9.8m/s^2 would be 980 or 3D4)
+#
+def getGravity():
+    global setup
+    if(not setup):
+        print("ERROR IMU has not been initialized")
+        return [-1, -1, -1]
+
+    XMSB = IMU.readByte(int("33", 16))
+    XLSB = IMU.readByte(int("32", 16))
+    YMSB = IMU.readByte(int("31", 16))
+    YLSB = IMU.readByte(int("30", 16))
+    ZMSB = IMU.readByte(int("2F", 16))
+    ZLSB = IMU.readByte(int("2E", 16))
+
+    print(XMSB)
+    print(XLSB)
+    print(YMSB)
+    print(YLSB)
+    print(ZMSB)
+    print(ZLSB)
+
+
+setupIMU()
+getGravity()
