518bfbf3083c17fa5d70a83e77754af098da0ab7
etchil <ethanchilderhose@gmail.com>
2022-08-11 14:37:59 -0400

    up

--- TorqueTest.py
+++ TorqueTest.py
@@ -1,31 +1,37 @@
+import sys
+sys.path.append("home/bracketbot/Desktop/BracketBot/")
 import BigCreteSpi as theCrete
-<<<<<<< HEAD
-import 4671RegDict as d
-=======
->>>>>>> 4a12961b533b27fa99cc2f88e2566ce2da8b4f02
 import time
 import os
+
+import refridge as d
 
 Lspi4671 = theCrete.BIG_CRETE_SPI(13, 12, 11, 16)
 Lspi6100 = theCrete.BIG_CRETE_SPI(13, 12, 11, 7)
 Rspi4671 = theCrete.BIG_CRETE_SPI(36, 38, 37, 40)
 Rspi6100 = theCrete.BIG_CRETE_SPI(36, 38, 37, 35)
 
-<<<<<<< HEAD
-Lspi4671.writeByte(d.regs_4671["TMC4671_MOTOR_TYPE_N_POLE_PAIRS"],  [0x00,0x03,0x00,0x0F])
-Lspi4671.writeByte(d.regs_4671["TMC4671_PWM_POLARITIES"],           [0x00,0x00,0x00,0x00])
-Lspi4671.writeByte(d.regs_4671["TMC4671_PWM_MAXCNT"],               [0x00,0x00,0x0F,0x9F])
-Lspi4671.writeByte(d.regs_4671["TMC4671_PWM_BBM_H_BBM_L"],          [0x00,0x00,0x28,0x28])
-Lspi4671.writeByte(d.regs_4671["TMC4671_PWM_SV_CHOP"],              [0x00,0x00,0x00,0x07])
+Lspi6100.writeByte(0x01, [255,255,255,255])
+Lspi6100.writeByte(0x00, [00,00,00,00])
+data = Lspi6100.readByte(0x0A)
+data[0] = 00
+data[1] = 00
+Lspi6100.writeByte(0x0A, data)
+
+Lspi4671.writeByte(d.regs_4671["MOTOR_TYPE_N_POLE_PAIRS"],  [0x00,0x03,0x00,0x0F])
+Lspi4671.writeByte(d.regs_4671["PWM_POLARITIES"],           [0x00,0x00,0x00,0x00])
+Lspi4671.writeByte(d.regs_4671["PWM_MAXCNT"],               [0x00,0x00,0x0F,0x9F])
+Lspi4671.writeByte(d.regs_4671["PWM_BBM_H_BBM_L"],          [0x00,0x00,0x28,0x28])
+Lspi4671.writeByte(d.regs_4671["PWM_SV_CHOP"],              [0x00,0x00,0x00,0x07])
 
 #ADC configuration
-Lspi4671.writeByte(d.regs_4671["TMC4671_ADC_I_SELECT"],             [0x18,0x00,0x01,0x00])
-Lspi4671.writeByte(d.regs_4671["TMC4671_dsADC_MCFG_B_MCFG_A"],      [0x00,0x10,0x00,0x10])
-Lspi4671.writeByte(d.regs_4671["TMC4671_dsADC_MCLK_A"],             [0x20,0x00,0x00,0x00])
-Lspi4671.writeByte(d.regs_4671["TMC4671_dsADC_MCLK_B"],             [0x20,0x00,0x00,0x00])
-Lspi4671.writeByte(d.regs_4671["TMC4671_dsADC_MDEC_B_MDEC_A"],      [0x01,0x4E,0x01,0x4E])
-Lspi4671.writeByte(d.regs_4671["TMC4671_ADC_I0_SCALE_OFFSET"],      [0x01,0x00,0x81,0x3F])
-Lspi4671.writeByte(d.regs_4671["TMC4671_ADC_I1_SCALE_OFFSET"],      [0x01,0x00,0x81,0xA8])
+Lspi4671.writeByte(d.regs_4671["ADC_I_SELECT"],             [0x18,0x00,0x01,0x00])
+Lspi4671.writeByte(d.regs_4671["dsADC_MCFG_B_MCFG_A"],      [0x00,0x10,0x00,0x10])
+Lspi4671.writeByte(d.regs_4671["dsADC_MCLK_A"],             [0x20,0x00,0x00,0x00])
+Lspi4671.writeByte(d.regs_4671["dsADC_MCLK_B"],             [0x20,0x00,0x00,0x00])
+Lspi4671.writeByte(d.regs_4671["dsADC_MDEC_B_MDEC_A"],      [0x01,0x4E,0x01,0x4E])
+Lspi4671.writeByte(d.regs_4671["ADC_I0_SCALE_OFFSET"],      [0x01,0x00,0x81,0x3F])
+Lspi4671.writeByte(d.regs_4671["ADC_I1_SCALE_OFFSET"],      [0x01,0x00,0x81,0xA8])
 
 #ABN Encoder settings
 Lspi4671.writeByte(d.regs_4671["ABN_DECODER_MODE"],                 [0x00,0x00,0x10,0x00])
@@ -41,6 +47,7 @@ Lspi4671.writeByte(d.regs_4671["PID_TORQUE_P_TORQUE_I"],            [0x01,0x00,0
 Lspi4671.writeByte(d.regs_4671["PID_FLUX_P_FLUX_I"],                [0x01,0x00,0x01,0x00])
 
 # Encoder init
+print("Initializing Encoder...")
 Lspi4671.writeByte(d.regs_4671["MODE_RAMP_MODE_MOTION"],            [0x00,0x00,0x00,0x08])
 Lspi4671.writeByte(d.regs_4671["ABN_DECODER_PHI_E_PHI_M_OFFSET"],   [0x00,0x00,0x00,0x00])
 Lspi4671.writeByte(d.regs_4671["PHI_E_SELECTION"],                  [0x00,0x00,0x00,0x01])
@@ -48,24 +55,24 @@ Lspi4671.writeByte(d.regs_4671["PHI_E_EXT"],                        [0x00,0x00,0
 Lspi4671.writeByte(d.regs_4671["UQ_UD_EXT"],                        [0x00,0x00,0x0F,0xA0])
 time.sleep(1)
 Lspi4671.writeByte(d.regs_4671["ABN_DECODER_COUNT"],                [0x00,0x00,0x00,0x00])
-
+print("Encoder Initialized")
 #Feedback selection
 Lspi4671.writeByte(d.regs_4671["PHI_E_SELECTION"],                  [0x00,0x00,0x00,0x03])
 Lspi4671.writeByte(d.regs_4671["VELOCITY_SELECTION"],               [0x00,0x00,0x00,0x09])
 
 # Torque mode
 Lspi4671.writeByte(d.regs_4671["MODE_RAMP_MODE_MOTION"],            [0x00,0x00,0x00,0x01])
-
+print("Rotate Right")
 # Rotate right
 Lspi4671.writeByte(d.regs_4671["PID_TORQUE_FLUX_TARGET"],           [0x03,0xE8,0x00,0x00])
 time.sleep(1)
 
 # Rotate left
+print("Rotate Left")
 Lspi4671.writeByte(d.regs_4671["PID_TORQUE_FLUX_TARGET"],           [0xFC,0x18,0x00,0x00])
 time.sleep(1)
 
 #Stop
 Lspi4671.writeByte(d.regs_4671["PID_TORQUE_FLUX_TARGET"],           [0x00,0x00,0x00,0x00])
-=======
+time.sleep(1)
 
->>>>>>> 4a12961b533b27fa99cc2f88e2566ce2da8b4f02
