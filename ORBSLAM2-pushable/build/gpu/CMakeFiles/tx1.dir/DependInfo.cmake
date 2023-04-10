623a50e01c4d8c5cfefc40fbf9be6b83843693f0
etchil <ethanchilderhose@gmail.com>
2023-04-09 22:52:49 -0400

    idk

---
+++ ORBSLAM2-pushable/build/gpu/CMakeFiles/tx1.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bracketbot/Desktop/BracketBot/ORB_SLAM2/gpu/tx1.cpp" "/home/bracketbot/Desktop/BracketBot/ORB_SLAM2/build/gpu/CMakeFiles/tx1.dir/tx1.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "HAVE_EIGEN"
+  "HAVE_GLEW"
+  "PANGO_DEFAULT_WIN_URI=\"wayland\""
+  "_LINUX_"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../"
+  "../include"
+  "/usr/local/include/eigen3"
+  "/usr/local/cuda/include"
+  "/usr/local/include/opencv4"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/bracketbot/Desktop/BracketBot/ORB_SLAM2/build/CMakeFiles/ORB_SLAM2.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
